ba26f417bc6aa5bd5a6d69413692b0a3814e9916
Chace DeLoach <shinobi2233@yahoo.com>
2020-09-15 18:48:11 -0400

    add mobile menu

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,3 +1,10 @@
+const menuBtn = document.querySelector(".menu-btn");
+const burger = document.querySelector(".menu-btn-burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav-item");
+let showMenu = false;
+
 let articles = document.querySelectorAll("article");
 
 let i = 0;
@@ -8,3 +15,27 @@ let handler = setInterval(function () {
         clearInterval(handler);
     }
 }, 300);
+
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        burger.classList.add("open");
+        nav.classList.add("open");
+        menuNav.classList.add("open");
+        navItems.forEach(item => {
+            item.classList.add("open");
+        });
+        showMenu = true;
+    }
+    else {
+        burger.classList.remove("open");
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        navItems.forEach(item => {
+            item.classList.remove("open");
+        });
+        showMenu = false;
+    }
+}
